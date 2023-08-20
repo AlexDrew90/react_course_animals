0b6f8ae9649dea4d4dd0dd0f1e016e602f9a5a09
Alex Drew <alexander.william.drew@gmail.com>
2023-08-20 12:39:49 +0800

    Utilising .map function to take random animals and convert into JSX using the AnimalShow function from the child.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+const getRandomAnimal = () =>{
+
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+ return animals[Math.floor(Math.random() * animals.length)]
+}
 
 const App = () => {
 
-const [count, setCount] = useState(0);
+const [animals, setAnimals] = useState([]);
 
 const handleClick = () => {
-  setCount(count + 1);
+ setAnimals([...animals, getRandomAnimal()])
 };
+
+const renderedAnimals = animals.map((animal, index) => {return <AnimalShow type={animal} key={index} />});
 
   return(
   <div>
-  <button onClick={handleClick}>Add animal</button>;
-  <div>Number of animals is: {count}</div>
+  <button onClick={handleClick}>Add animal</button>
+  <div>{renderedAnimals}</div>
   </div>);
 }
 
